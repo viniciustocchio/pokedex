f2aafeb7ce521b68ecb35c4a5f0a17a5cf93a506
Vinicius Tocchio <80677157+vtocchio@users.noreply.github.com>
2022-06-20 15:03:34 +0200

    added array in script

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-alert('Hello world');
-
-let favoriteFood = "hamburger";
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Bulbasaur", height: 0.7, weight: 6.9, type: ["grass", "poison"] },
+  { name: "Charmander", height: 0.6, weight: 8.5, type: "fire" },
+  { name: "Squirtle", height: 0.5, weight: 9, type: "water" },
+  { name: "Caterpie", height: 0.3, weight: 2.9, type: "bug" },
+  { name: "Pidgey", height: 0.3, weight: 1.8, type: ["flying", "normal"] },
+  { name: "Pikachu", height: 0.4, weight: 6, type: "eletric" },
+];
